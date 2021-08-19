7c4aa5997f69118503a1ca24a8b2bc92be20c32d
suprit <supriths4685@gmail.com>
2021-08-20 02:37:42 +0530

    day 6 done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "suprit";
   @override
   Widget build(BuildContext context) {
-    int data = 30;
-    const String s = "suprit";
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text(" $s welcome $data"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatalogModel.items[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
